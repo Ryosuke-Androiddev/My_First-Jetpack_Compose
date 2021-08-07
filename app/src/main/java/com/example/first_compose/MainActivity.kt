a5bbcedfb65760b42154b40437605e41f351581d
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2021-08-07 21:51:14 +0900

    initial commit

---
+++ app/src/main/java/com/example/first_compose/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.first_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.first_compose.ui.theme.FirstComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirstComposeTheme {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FirstComposeTheme {
+        Greeting("Android")
+    }
+}
